7548219124e6af965ea1c80f48936f1f46a42aa6
d27khan <daniel.khan_@hotmail.com>
2023-01-11 16:44:28 -0500

    imread function

--- assignment1/assignment/io.py
+++ assignment1/assignment/io.py
@@ -19,9 +19,27 @@ def imread(filename):
     ValueError
         if the image format is unknown or invalid
     '''
-
-    raise NotImplementedError('Implement this function/method.')
-    return np.ndarray([1, 2, 3])
+    # Read in the file
+    with open(filename, 'rt') as f:
+        contents = f.read()
+    # Split the file contents into it's constituent tokens.
+    tokens = contents.split()
+    # checking P2 and P3 formats
+    if tokens[0] != ('P2' and 'P3'):
+        raise ValueError(f'Unknown format {tokens[0]}')
+    # Get the image dimensions.
+    width = int(tokens[1])
+    height = int(tokens[2])
+    maxval = int(tokens[3])
+    isP2 = 0 if tokens[0] == "P2" else 3
+    # Convert all of the string tokens into integers.
+    values = [int(token) for token in tokens[4:]]
+    if maxval != 255:
+        raise ValueError('Can only support 8-bit images.')
+    # Create the numpy array, reshaping it so that it's no longer a linear
+    # array.
+    image = np.array(values, dtype=np.uint8)
+    return np.reshape(image, (height, width, isP2))
 
 
 def imwrite(filename, image):
