f66bc7490fcf21ec20ebc36ced822c89229ba3dd
d27khan <daniel.khan_@hotmail.com>
2023-01-22 22:53:10 -0500

    io.py almost done, need to fix up read function

--- assignment1/assignment/io.py
+++ assignment1/assignment/io.py
@@ -42,6 +42,7 @@ def imread(filename):
     return np.reshape(image, (height, width, isP2))
 
 
+"""
 def imwrite(filename, image):
     '''Save a NetPBM image to a file.
 
@@ -57,24 +58,70 @@ def imwrite(filename, image):
         height, width = image.shape
         # Convert the image values to strings.
         values = image.astype(str)
-            # Construct the file contents.
+        # Construct the file contents.
         rows = [' '.join(row) for row in values]
         header = '\n'.join(['P2', str(width), str(height), "255"])
     elif image.ndim == 3:
         height, width, color = image.shape
         # Convert the image values to strings.
         values = image.astype(str)
-            # Construct the file contents.
+        # Construct the file contents.
         rows = [' '.join(row) for row in values]
         header = '\n'.join(['P3', str(width), str(height), "255"])
-        
-
     if image.dtype != np.uint8:
         raise ValueError('Can only support 8-bit images.')
-
     data = '\n'.join(rows)
     # Write it to a file.
     with open(filename, 'wt') as f:
         f.write(header)
         f.write('\n')
         f.write(data)
+"""
+
+
+def imwrite(filename, image):
+    '''Save a NetPBM image to a file.
+    Parameters
+    ----------
+    filename : str
+        image file name
+    image : numpy.ndarray
+        image being saved
+    '''
+    # Extract the image dimensions and check that it's 8bpc
+    if image.dtype != np.uint8:
+        raise ValueError('Can only support 8-bit images.')
+
+    if image.ndim == 2:
+        # Monochrome image
+        height, width = image.shape
+        # Construct the header for PGM format
+        header = '\n'.join(['P2', str(width), str(height), "255"])
+        # Convert the image values to strings.
+        values = image.astype(str)
+        # Construct the file contents.
+        rows = [' '.join(row) for row in values]
+    elif image.ndim == 3:
+        # Color image
+        height, width, color = image.shape
+        # Check if it's RGB
+        if color != 3:
+            raise ValueError('Only RGB images are supported')
+        # Construct the header for PPM format
+        header = '\n'.join(['P3', str(width), str(height), "255"])
+        # Interleave the channels
+        image = image.reshape(height, width * color)
+        # Convert the image values to strings.
+        values = image.astype(str)
+        # Construct the file contents.
+        rows = [' '.join(row) for row in values]
+    else:
+        raise ValueError('Image dimension not supported')
+
+    # Write it to a file.
+    with open(filename, 'wt') as f:
+        f.write(header)
+        f.write('\n')
+        for row in rows:
+            f.write(row)
+            f.write('\n')
